8f3aa339c9b601d1eb3eed20d2a46effaa04bcf7
Emily Domanico <56197359+emilydomanico@users.noreply.github.com>
2022-04-20 17:14:23 -0400

    stars to read in raster
    read in example raster and crop

--- analysis/ProcessConveyalTest.R
+++ analysis/ProcessConveyalTest.R
@@ -1,23 +1,24 @@
 # Raster processing
-library(raster)
-library(rgdal)
-
+library(tidyverse)
+library(sf)
+library(stars)
 
 # read in census geog layer
 mpo_census_tracts
 mpo_census_bgr
 
-bounary
+boundary<- read_rds("data/boundary.rds") %>% 
+  st_transform(26986)
 
 # read in geotiff
-access_layer<- raster(".geotiff")
-
+access_layer<- read_stars("data/ConveyalRuns/HealthCareTests/HealthCare_NonEmergency_AMPeak_TransitBusRT_45min_50pct.geotiff") %>% 
+  st_transform(26986)
+plot(access_layer)
 # crop raster by mpo census geog
-access_layer <- crop(access_layer, boundary)
+access_layer_crop <- st_crop(access_layer, boundary)
 
-plot(access_layer, main= "Access Layer Cropped by MPO boundary",
-     col = rev())
-plot(bounary, add= TRUE)
+plot(access_layer_crop, main= "Access Layer Cropped by MPO boundary")
+plot(boundary,col="blue", add= TRUE)
   
 
 
