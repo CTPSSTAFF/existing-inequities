9145e4f042da9009104a97f2b64ca91da843e31c
Emily Domanico <56197359+emilydomanico@users.noreply.github.com>
2022-05-10 12:08:06 -0400

    higher education destination prep
    match enrollment to campus points. save resutls.

--- analysis/DestDataPrep_HigherEd.R
+++ analysis/DestDataPrep_HigherEd.R
@@ -7,9 +7,9 @@ library(readxl)
 library(sf)
 library(mapview)
 `%notin%` <- Negate(`%in%`)
+source("functions/points_cleaning.R")
 
-boundary<- read_rds("data/boundary.rds") %>% 
-  st_transform(26986)
+boundary<- read_rds("data/boundary.rds")
 
 
 # STEP 1: read in data from the data folder ####
@@ -32,9 +32,6 @@ rm(shp)
 colleges <- colleges %>% 
   st_filter(boundary, .predicate = st_within)
 
-
-
-
 # Prep to join enrollment to colleges ####
 e_prep <- enrollment %>% 
   mutate(id = substr(NCES_ID,1,6)) %>% 
@@ -42,6 +39,19 @@ e_prep <- enrollment %>%
   mutate(e_name = case_when(
      e_name == "Boston University School of Medicine" ~ 	
       "Boston University School of Medicine (Medical Campus)",
+     # note Northeastern enrollment not provided by campus, so just using main campus
+     e_name == "Northeastern University"~ "Northeastern University (Main Campus)",
+     e_name == "University of Massachusetts Boston" ~ "University of Massachusetts Boston (Boston)",
+     e_name == "Cambridge College" ~ "Cambridge College (Main Campus)",
+     e_name == "Harvard University (Main Campus)" ~ "Harvard College (Main Campus)",
+     e_name == "Massachusetts College of Pharmacy and Health Science" ~ "Massachusetts College of Pharmacy and Health Science (Boston)",
+     e_name == "Massasoit Community College" ~ "Massasoit Community College (Canton)",
+     e_name == "Gordon-Conwell Theological Seminary" ~ "Gordon-Conwell Theological Seminary (Boston)",
+     e_name == "Massachusetts Bay Community College" ~ "Massachusetts Bay Community College (Ashland - Auto Tech Center)",
+     e_name == "North Shore Community College (Danvers Campus)"~ "North Shore Community College (Danvers)",
+     e_name == "Simmons College" ~"Simmons College (Main Campus)",
+     # note Spa Tech Institute enrollment assigned to Ipswich campus point
+     e_name == "Spa Tech Institute" ~ "Spa Tech Institute (Ipswich)",
      TRUE ~ e_name))
 c_prep <- colleges %>% 
   mutate(id = substr(NCES_ID, 1, 6)) %>% 
@@ -49,21 +59,28 @@ c_prep <- colleges %>%
   select(c_name, COLLEGE, CAMPUS, id, c_NCES_ID= NCES_ID) 
 
 ec <- e_prep %>% 
-  # left_join(c_prep, by= c("id"= "id")) %>%
-#    mutate(flag =ifelse(e_name != c_name, T, F))
     left_join(c_prep, by= c("id"= "id", "e_name"= "c_name")) %>% 
-# 
-# test <- e_prep %>% 
-#   anti_join(c_prep, by= c("id"= "id", "e_name"= "c_name"))
+    arrange(id)%>% 
+    filter(tot_enroll_19 >0) %>% 
+    st_as_sf() %>% 
+    select(name = e_name, enrollment= tot_enroll_19, NCES= e_NCES_ID) %>% 
+    mutate(id = row_number(),
+           type = "higherEd")
 
+# Some campus points missing enrollment are not graduate education opportunities
+# some campus points missing enrollment had enrollment assigned to the main campus, as it wasn't reported by campus.
+c_no_enrollment <- c_prep %>% 
+  anti_join(e_prep,by= c("id"= "id", "c_name"= "e_name"))
 
-  # group_by(id) %>% 
-  arrange(id) #%>%
+mapview(ec, cex = 10, color = "red")+ mapview(c_no_enrollment, cex= 4)
 
-ec_issue <- ec %>% 
-  filter(is.na(c_NCES_ID)==T)
+# SAVE DATA ####
+st_write(ec, "output/DestinationData.gpkg", "higherEd_PT")
 
-ec_check <- ec %>% 
-  filter(is.na(c_NCES_ID)==F) %>% 
-  st_as_sf()
-mapview(ec_check, cex = "tot_enroll_19")+ boundary
+# SAVE DATA FOR CONVEYAL ####
+ed_conveyal <- st_read("output/DestinationData.gpkg", "higherEd_PT") %>% 
+  rename(weight = enrollment) %>% 
+  prep_pt_to_csv_keepID_weight()
+
+ed_csv<- ed_conveyal %>%   
+  pt_to_csv("output/higherEd.csv")
