51183fe3a3863e0cf0f673b8d89ba000b838facd
Emily Domanico <56197359+emilydomanico@users.noreply.github.com>
2022-08-15 15:38:45 -0400

    update comments

--- analysis/TravelCosts_OriginDestProcess.R
+++ analysis/TravelCosts_OriginDestProcess.R
@@ -5,6 +5,7 @@ library(mapview)
 # Travel time costs 
 # Value of travel time (VTT) calculated 
 # https://www.vtpi.org/tca/tca0502.pdf
+# see table 17: Table 17 https://www.vtpi.org/traveltime.pdf
 
 # read in conveyal outputs drive times and transit paths
 drive_times <- read_csv("data/ConveyalRuns/TravelCosts_OD_Drive_AMPeak_TIMES.csv")
@@ -61,7 +62,8 @@ transit_paths <- transit_paths %>%
 # median earnings mpo $56,507.64 (table B08119)
 # median hh inc mpo $97,085.11  (table B19001)
 wage_rate_annual <- 56507.64
-wage_rate<- wage_rate_annual/(40*52) # assuming 40hr work week and 52 weeks in a
+wage_rate<- wage_rate_annual/(40*52) # assuming 40hr work week and 52 weeks in a year
+
 
 price_travel_transit<- function(
                         time_walk_to_stop,
@@ -80,6 +82,13 @@ price_travel_transit<- function(
     time_walk_to_destination/60*wage_rate_hrly*.7, na.rm = T)
   return(cost)
 }
+price_travel_drive <- function(wage_rate_hrly= wage_rate, 
+                               time_in_vehicle) {
+  cost <- sum(
+    time_in_vehicle/60*wage_rate_hrly*.35, na.rm = T)
+  return(cost)
+}
+
 price_travel_drive <- function(wage_rate_hrly= wage_rate, 
                                time_in_vehicle) {
   cost <- sum(
